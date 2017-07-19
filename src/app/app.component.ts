955e78987d4cd2cd7c0d23fe2f1e8abcc87180c3
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-07-19 16:44:43 -0700

    no outline for project arrow icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  isActive = '';
-
-  onHamburger() {
-    this.isActive == '' ? this.isActive = 'is-active' : this.isActive = '';
-    console.log(document.getElementById('hamburger-5'));
-  }
+  
 }
