7561322e720429afc7c1ae27dbce4eb0a81d0842
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-07-31 16:25:35 -0700

    made changes to skills and tools

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,28 +8,28 @@ import { Item } from './item.model';
 })
 export class SkillsComponent implements OnInit {
 
-  icons: Item[] = [
-    new Item(
-      'angular-icon.svg',
-      'https://www.google.com'
-    ),
-    new Item(
-      'aws-icon.png',
-      'https://www.google.com'
-    ),
-    new Item(
-      'cordova-icon.png',
-      'https://www.google.com'
-    ),
-    new Item(
-      'firebase-icon.png',
-      'https://www.google.com'
-    ),
-    new Item(
-      'heroku-icon.png',
-      'https://www.google.com'
-    )
-  ];
+  // icons: Item[] = [
+  //   new Item(
+  //     'angular-icon.svg',
+  //     'https://www.google.com'
+  //   ),
+  //   new Item(
+  //     'aws-icon.png',
+  //     'https://www.google.com'
+  //   ),
+  //   new Item(
+  //     'cordova-icon.png',
+  //     'https://www.google.com'
+  //   ),
+  //   new Item(
+  //     'firebase-icon.png',
+  //     'https://www.google.com'
+  //   ),
+  //   new Item(
+  //     'heroku-icon.png',
+  //     'https://www.google.com'
+  //   )
+  // ];
   
   constructor() { }
 
