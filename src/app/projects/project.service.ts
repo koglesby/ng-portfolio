6eed6255e32b2a11a3af6b738ffddb0ad580da69
Kevin Oglesby <kevinjoglesby@gmail.com>
2019-03-11 14:38:49 -0700

    added node chat app project

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -14,9 +14,9 @@ export class ProjectService {
     new Project(
       'Agile Team Project / Chess App',
       'Worked on an Agile team of 5 developers to build a Ruby on Rails' +
-        ' chess application that enforces the rules of the game. ' +
-        'Under the guidance of a senior software engineer, we ' +
-        'had weekly Agile team meetings, code reviews and sprint planning.',
+      ' chess application that enforces the rules of the game. ' +
+      'Under the guidance of a senior software engineer, we ' +
+      'had weekly Agile team meetings, code reviews and sprint planning.',
       '../assets/chess.jpg',
       'https://github.com/8th-Rank-Pawns/chess-app',
       'http://chess-app-8th-rank-pawns.herokuapp.com'
@@ -24,7 +24,7 @@ export class ProjectService {
     new Project(
       'MERN Stack Developer Network',
       'Full stack social network application built using Node.js, Express, React, Redux and MongoDB. ' +
-        'Backend api tested during development using Postman. Routes and endpoints protected with JSON Web Tokens.',
+      'Backend api tested during development using Postman. Routes and endpoints protected with JSON Web Tokens.',
       '../assets/devconnect.jpg',
       'https://github.com/koglesby/devconnect',
       'https://devconnect-koglesby.herokuapp.com/'
@@ -32,8 +32,8 @@ export class ProjectService {
     new Project(
       'React + Redux Burger-Builder',
       'Create custom burgers with automatic price adjustment. Employs a firebase backend with' +
-        'user authentication and forms for contact information. Built using the React library with ' +
-        'highly reusable components and Redux for state management.',
+      'user authentication and forms for contact information. Built using the React library with ' +
+      'highly reusable components and Redux for state management.',
       '../assets/burgerbuilder.jpg',
       'https://github.com/koglesby/burger-builder',
       'https://koglesby-react-burger-builder.firebaseapp.com/'
@@ -41,8 +41,8 @@ export class ProjectService {
     new Project(
       'React Native Places App',
       'Built using React Native and Redux, this mobile app makes use of native device features, allowing' +
-        ' users to quickly take a picture or select one from their library to upload and share, and add a location ' +
-        "using the device's currrent position or one selected from a map view component.",
+      ' users to quickly take a picture or select one from their library to upload and share, and add a location ' +
+      "using the device's currrent position or one selected from a map view component.",
       '../assets/reactnativeplaces.jpg',
       'https://github.com/koglesby/react-native-places',
       ''
@@ -50,9 +50,9 @@ export class ProjectService {
     new Project(
       'Angular Single Page Portfolio',
       "You're looking at this app right now! This app was built from scratch using the Angular 4 framework" +
-        ' and features a responsive design which incorporates Angular animations, ' +
-        'and makes use of observables and services in order to pass data between components' +
-        ' and allow for simple project additions and edits.',
+      ' and features a responsive design which incorporates Angular animations, ' +
+      'and makes use of observables and services in order to pass data between components' +
+      ' and allow for simple project additions and edits.',
       '../assets/portfolio.jpg',
       'https://github.com/koglesby/ng-portfolio',
       ''
@@ -68,9 +68,9 @@ export class ProjectService {
     new Project(
       'Angular + Rails Freelancing App',
       'A freelancing app which combines the Angular front-end framework ' +
-        'with two Rails 5 API apps functioning as a backend. This architecture allows for' +
-        ' automatic updates of page elements without page refresh, while' +
-        ' leveraging the power and flexibility of the Rails.',
+      'with two Rails 5 API apps functioning as a backend. This architecture allows for' +
+      ' automatic updates of page elements without page refresh, while' +
+      ' leveraging the power and flexibility of the Rails.',
       '../assets/freelance-camp.jpg',
       'https://github.com/koglesby/freelance-camp',
       'http://koglesby-freelance-camp.herokuapp.com/home'
@@ -78,16 +78,23 @@ export class ProjectService {
     new Project(
       'Angular + Firebase Recipe App',
       'A single-page application built using the Angular 4 framework. ' +
-        'Integrated with Firebase to store and retrieve recipe and ' +
-        'shopping list data in real time.',
+      'Integrated with Firebase to store and retrieve recipe and ' +
+      'shopping list data in real time.',
       '../assets/reciperoost.jpg',
       'https://github.com/koglesby/reciperoost',
       'http://ng-reciperoost-deployment.s3-website-us-west-1.amazonaws.com/'
     ),
     new Project(
+      'Express Chat App',
+      'A chat app that uses an express server with socket.io to send messages in real time. Tested thoroughly with Mocha.',
+      '../assets/node-chat.jpg',
+      'https://github.com/koglesby/node-chat-app',
+      'https://koglesby-node-chat.herokuapp.com/'
+    ),
+    new Project(
       'Instagram Clone / TDD',
       'An Instagram clone that was built using industry-standard, test-driven ' +
-        'development following numerous red/green/refactor cycles.',
+      'development following numerous red/green/refactor cycles.',
       '../assets/grammable.jpg',
       'https://github.com/koglesby/grammable',
       'http://grammable-kevin-oglesby.herokuapp.com/'
@@ -95,9 +102,9 @@ export class ProjectService {
     new Project(
       'Online Marketplace',
       'A two-sided, video-streaming marketplace platform that' +
-        ' features credit card payment capabilities, user role' +
-        ' management, complex user interfaces, and advanced' +
-        ' database relationships.',
+      ' features credit card payment capabilities, user role' +
+      ' management, complex user interfaces, and advanced' +
+      ' database relationships.',
       '../assets/flixter.jpg',
       'https://github.com/koglesby/flixter',
       'http://flixter-kevin-oglesby.herokuapp.com'
@@ -105,8 +112,8 @@ export class ProjectService {
     new Project(
       'Yelp Clone',
       'A Yelp clone that integrates with the Google Maps API ' +
-        'and includes features like user comments, star ratings, ' +
-        'image uploading, and user authentication.',
+      'and includes features like user comments, star ratings, ' +
+      'image uploading, and user authentication.',
       '../assets/nomster.jpg',
       'https://github.com/koglesby/nomly',
       'http://nomly-kevin-oglesby.herokuapp.com'
@@ -120,15 +127,15 @@ export class ProjectService {
     //   'https://github.com/koglesby/todo',
     //   'http://todo-kevin-oglesby.herokuapp.com'
     // ),
-    new Project(
-      'Quote Generator',
-      'This is the first full Web App that I built. It is a database-powered quote generator with a mobile-first' +
-        ' design, using the Ruby on Rails framework, HTML, and CSS. ' +
-        ' It uses Git and GitHub for version control, and was launched on Heroku.',
-      '../assets/splurty.png',
-      'https://github.com/koglesby/splurty',
-      'http://splurty-kevin-oglesby.herokuapp.com/'
-    )
+    // new Project(
+    //   'Quote Generator',
+    //   'This is the first full Web App that I built. It is a database-powered quote generator with a mobile-first' +
+    //     ' design, using the Ruby on Rails framework, HTML, and CSS. ' +
+    //     ' It uses Git and GitHub for version control, and was launched on Heroku.',
+    //   '../assets/splurty.png',
+    //   'https://github.com/koglesby/splurty',
+    //   'http://splurty-kevin-oglesby.herokuapp.com/'
+    // )
 
     // new Project(
     //   'Ionic App',
